079630c0892c278d04452da45f285c7e99df7142
Ira-sy <58573365+Ira-sy@users.noreply.github.com>
2021-05-23 23:11:50 +0300

    Add files via upload

---
+++ DBLibraryContext.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace LibraryWebApplication
+{
+    public partial class DBLibraryContext : DbContext
+    {
+        public DBLibraryContext()
+        {
+        }
+
+        public DBLibraryContext(DbContextOptions<DBLibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<AuthorsBook> AuthorsBooks { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Reader> Readers { get; set; }
+        public virtual DbSet<ReadersBook> ReadersBooks { get; set; }
+        public virtual DbSet<Status> Statuses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server= DESKTOP-DUBM1KF\\SQLEXPRESS; Database=DBLibrary; Trusted_Connection=True; ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Info).HasColumnType("ntext");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsFixedLength(true);
+            });
+
+            modelBuilder.Entity<AuthorsBook>(entity =>
+            {
+                entity.Property(e => e.AuthorId).HasColumnName("AuthorID");
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.AuthorsBooks)
+                    .HasForeignKey(d => d.AuthorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AuthorsBooks_Authors");
+
+                entity.HasOne(d => d.AuthorNavigation)
+                    .WithMany(p => p.AuthorsBooks)
+                    .HasForeignKey(d => d.AuthorId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AuthorsBooks_Books1");
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Info).HasColumnType("ntext");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Books_DIC_Categories");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Info).HasColumnType("text");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Reader>(entity =>
+            {
+                entity.Property(e => e.Address).HasColumnType("ntext");
+
+                entity.Property(e => e.Info).HasColumnType("ntext");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<ReadersBook>(entity =>
+            {
+                entity.Property(e => e.FactReturn).HasColumnType("date");
+
+                entity.Property(e => e.Issue).HasColumnType("date");
+
+                entity.Property(e => e.PlanReturn).HasColumnType("date");
+
+                entity.HasOne(d => d.Book)
+                    .WithMany(p => p.ReadersBooks)
+                    .HasForeignKey(d => d.BookId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ReadersBooks_Books1");
+
+                entity.HasOne(d => d.Reader)
+                    .WithMany(p => p.ReadersBooks)
+                    .HasForeignKey(d => d.ReaderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ReadersBooks_Readers");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.ReadersBooks)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ReadersBooks_Statuses");
+            });
+
+            modelBuilder.Entity<Status>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
